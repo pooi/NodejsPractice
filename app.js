7e38c5a0c3b23df0cb5ae4c12d92e1d606b4a98b
pooi <ldayou@naver.com>
2017-12-27 10:42:47 +0900

    Add files
      * Learning static and dynamic expression.
      * Added public directory.

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    for (var i = 0; i < 5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+
+        <body>
+            Hello Dynamic!
+            <ul>
+                ${lis}
+            </ul>
+            ${time}
+        </body>
+
+        </html>
+    `;
+    res.send(output);
+});
+
 app.get('/home', function(req, res) {
     res.send('Hello World2!');
+});
+
+app.get('/router', function(req, res) {
+    res.send('Hello Router, <img src="/router.png">');
 });
 
 app.get('/login', function(req, res) {
