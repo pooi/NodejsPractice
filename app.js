29fa37d7289f2f1d79f7e76ba441834cf0f23995
pooi <ldayou@naver.com>
2017-12-30 12:49:37 +0100

    Add files
      * Learning form tag.
      * Learning post and get method
      * Added body-parser module.

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
+});
+
+app.get('/form', function(req, res) {
+    res.render('form');
+});
+
+app.get('/form_receiver', function(req, res) {
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send(title + ', ' + description);
+});
+
+app.post('/form_receiver', function(req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title + ', ' + description);
 });
 
 app.get('/topic', function(req, res) {
