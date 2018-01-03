ea892b448fe27e311b210a2073a5f39c6397e09d
pooi <ldayou@naver.com>
2018-01-03 15:07:07 +0900

    Add files
      * Learning multiple router from one get method.
      * Learning supervisor package

--- app_file.js
+++ app_file.js
@@ -9,35 +9,37 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/topic/new', function(req, res) {
-    res.render('new');
-});
 
-app.get('/topic', function(req, res) {
     fs.readdir('data', function(err, files) {
         if (err) {
             console.log(err);
             res.status(500).send("Internal Server Error");
         }
-        res.render('view', { topics: files });
+        res.render('new', { topics: files });
     });
 });
 
-app.get('/topic/:id', function(req, res) {
-    var id = req.params.id;
+app.get(['/topic', '/topic/:id'], function(req, res) {
     fs.readdir('data', function(err, files) {
         if (err) {
             console.log(err);
             res.status(500).send("Internal Server Error");
         }
-        fs.readFile('data/' + id, 'utf8', function(err, data) {
-            if (err) {
-                console.log(err);
-                res.status(500).send("Internal Server Error");
-            }
-            res.render('view', { topics: files, title: id, description: data });
-        });
+        var id = req.params.id;
+        if (id) {
+            // id 값이 있을 때
+            fs.readFile('data/' + id, 'utf8', function(err, data) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Internal Server Error");
+                }
+                res.render('view', { topics: files, title: id, description: data });
+            });
+        } else {
+            // id 값이 없을 때
+            res.render('view', { topics: files, title: 'Welcome', description: 'Hello, JavaScript for Server' });
+        }
     });
-
 });
 
 app.post('/topic', function(req, res) {
@@ -49,7 +51,7 @@ app.post('/topic', function(req, res) {
             console.log(err);
             res.status(500).send("Internal Server Error");
         }
-        res.send("Success!");
+        res.redirect('/topic/' + title);
     });
 });
 
