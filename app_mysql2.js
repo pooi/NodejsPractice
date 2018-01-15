40531667c6632da8bfc462fd5784757448e27fa5
pooi <ldayou@naver.com>
2018-01-15 13:15:06 +0900

    Add files   * Learning how to display the query results on web page.

---
+++ app_mysql2.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '123456',
+    database: 'o2'
+});
+conn.connect();
+
+app.locals.pretty = true;
+app.set('views', './views_mysql');
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/topic/new', function(req, res) {
+
+    fs.readdir('data', function(err, files) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+        }
+        res.render('new', { topics: files });
+    });
+});
+
+app.get('/test', function(req, res) {
+    res.render('test');
+});
+
+app.get(['/topic', '/topic/:id'], function(req, res) {
+    var sql = 'SELECT id, title FROM topic';
+    conn.query(sql, function(err, topics, fields) {
+        var id = req.params.id;
+        if (id) {
+            var sql = 'SELECT * FROM topic WHERE id=?';
+            conn.query(sql, [id], function(err, topic, fields) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Internal Server Error");
+                } else {
+                    res.render('view', { topics: topics, topic: topic[0] });
+                }
+            });
+        } else {
+            res.render('view', { topics: topics });
+        }
+
+    });
+    // fs.readdir('data', function(err, files) {
+    //     if (err) {
+    //         console.log(err);
+    //         res.status(500).send("Internal Server Error");
+    //     }
+    //     var id = req.params.id;
+    //     if (id) {
+    //         // id 값이 있을 때
+    //         fs.readFile('data/' + id, 'utf8', function(err, data) {
+    //             if (err) {
+    //                 console.log(err);
+    //                 res.status(500).send("Internal Server Error");
+    //             }
+    //             res.render('view', { topics: files, title: id, description: data });
+    //         });
+    //     } else {
+    //         // id 값이 없을 때
+    //         res.render('view', { topics: files, title: 'Welcome', description: 'Hello, JavaScript for Server' });
+    //     }
+    // });
+});
+
+app.post('/topic', function(req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+
+    fs.writeFile('data/' + title, description, function(err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+        }
+        res.redirect('/topic/' + title);
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Connected, 3000 port!')
+});
